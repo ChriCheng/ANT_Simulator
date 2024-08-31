d8d2c2f982b53f9df5631cc322858f01a38919b6
ChriCheng <762968511@qq.com>
2024-08-31 09:44:49 +0800

    Change the path in train_alexnet.py to correct path

--- ant_quantization/ImageNet/train_alexnet.py
+++ ant_quantization/ImageNet/train_alexnet.py
@@ -84,7 +84,7 @@ test_transform = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))  # CIFAR-10的均值和标准差
 ])
 
-test_dataset = datasets.CIFAR10(root='./DataSet/Cifar10', train=False, download=True, transform=test_transform)
+test_dataset = datasets.CIFAR10(root='../DataSet/Cifar10', train=False, download=True, transform=test_transform)
 test_loader = DataLoader(test_dataset, batch_size=256, shuffle=False, num_workers=2)
 print("量化前test")
 test_model_accuracy(model,test_loader,device)
